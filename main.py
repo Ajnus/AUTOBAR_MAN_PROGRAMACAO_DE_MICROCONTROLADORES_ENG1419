04eafe956eef385ee1764b0ebf29cccf7ac44ebf
Jam Ajna Soares <ajnasoares@hotmail.com>
2023-11-20 00:26:52 -0300

    initial commit, cria Janela com Título e um Label

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+window.title("Auto Barman")
+window.geometry('340x440')
+#window.configure(bg = '#333333' )
+
+label = tk.Label(window, text="Login")
+label.pack()
+
+window.mainloop()
+
